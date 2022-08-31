ad914efeee8c1b4173bce7cadcd6fd8de766021c
David Danier <danier@team23.de>
2022-08-31 22:43:01 +0200

    Add more tests

--- tests/test_changedetect.py
+++ tests/test_changedetect.py
@@ -1,6 +1,8 @@
-from typing import Dict, List
+import pickle
+from typing import Any, Dict, List, Optional, Tuple
 
 import pydantic
+import pytest
 
 from pydantic_changedetect import ChangeDetectionMixin
 
@@ -21,8 +23,10 @@ class NestedDict(ChangeDetectionMixin, pydantic.BaseModel):
     sub: Dict[str, Something]
 
 
-class UndetectableChange(ChangeDetectionMixin, pydantic.BaseModel):
-    sub: List[str]
+class SomethingWithBrokenPickleState(Something):
+    def __getstate__(self) -> Dict[str, Any]:
+        # Skip adding changed state in ChangedDetectionMixin.__getstate__
+        return super(ChangeDetectionMixin, self).__getstate__()
 
 
 def test_initial_state():
@@ -61,6 +65,20 @@ def test_set_changed_state_with_fixed_original():
     assert obj.has_changed
     assert obj.__original__ == {"id": 7}
     assert obj.__changed_fields__ == {"id"}
+
+
+def test_set_changed_will_disallow_invalid_parameters():
+    obj = Something(id=1)
+
+    with pytest.raises(RuntimeError):
+        obj.set_changed("id", "id", original=7)
+
+
+def test_set_changed_will_disallow_invalid_field_names():
+    obj = Something(id=1)
+
+    with pytest.raises(AttributeError):
+        obj.set_changed("invalid_field_name")
 
 
 def test_copy_keeps_state():
@@ -120,8 +138,6 @@ def test_changed_base_is_resetable():
 
 
 def test_pickle_keeps_state():
-    import pickle
-
     obj = Something(id=1)
 
     assert not pickle.loads(pickle.dumps(obj)).has_changed
@@ -131,6 +147,15 @@ def test_pickle_keeps_state():
 
     assert pickle.loads(pickle.dumps(obj)).has_changed
     assert pickle.loads(pickle.dumps(obj)).__changed_fields__ == {"id"}
+
+
+def test_pickle_even_works_when_changed_state_is_missing():
+    obj = SomethingWithBrokenPickleState(id=1)
+    obj.id = 2
+
+    # Now we cannot use the changed state, but nothing fails
+    assert not pickle.loads(pickle.dumps(obj)).has_changed
+    assert pickle.loads(pickle.dumps(obj)).__changed_fields__ == set()
 
 
 def test_stores_original():
@@ -204,3 +229,45 @@ def test_nested_dict():
     assert parent.__changed_fields__ == {'sub'}
     assert parent.__changed_fields_recursive__ == {'sub', 'sub.something', 'sub.something.id'}
     assert parent.__self_changed_fields__ == set()
+
+
+def test_unsupported_nested_type():
+    class NestedTuple(ChangeDetectionMixin, pydantic.BaseModel):
+        sub: Tuple[Something]
+
+    something = Something(id=1)
+    parent = NestedTuple(sub=(something,))
+
+    # Nothing changed so far
+    assert something.has_changed is False
+    assert parent.has_changed is False
+
+    # Change something inside parent, but unsupported outer type
+    parent.sub[0].id = 2
+    assert parent.sub[0].has_changed is True
+    assert parent.has_changed is False
+    assert parent.__changed_fields__ == set()
+    assert parent.__changed_fields_recursive__ == set()
+
+
+def test_use_private_attributes_works():
+    class SomethingPrivate(Something):
+        _private: Optional[int] = pydantic.PrivateAttr(None)
+
+    something = SomethingPrivate(id=1)
+
+    assert something.has_changed is False
+
+    something._private = 1
+
+    assert something.has_changed is False
+
+
+def test_construct_works():
+    something = Something.construct(id=1)
+
+    assert something.has_changed is False
+
+    something.id = 2
+
+    assert something.has_changed is True
