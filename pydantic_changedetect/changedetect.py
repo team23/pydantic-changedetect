e5e602e5694d7421dc5c119c7617b2d24036083e
David Danier <danier@team23.de>
2023-09-04 08:49:25 +0100

    chore: ♻️ Remove now unnecessary noqa comments

--- pydantic_changedetect/changedetect.py
+++ pydantic_changedetect/changedetect.py
@@ -490,7 +490,7 @@ class ChangeDetectionMixin(pydantic.BaseModel):
             object.__setattr__(clone, "model_self_changed_fields", self.model_self_changed_fields.copy())
             return clone
 
-        def dict(  # type: ignore[misc]  # noqa: F811
+        def dict(  # type: ignore[misc]
             self,
             *,
             include: Optional[Union['AbstractSetIntStr', 'MappingIntStrAny']] = None,
@@ -520,7 +520,7 @@ class ChangeDetectionMixin(pydantic.BaseModel):
                 ),
             )
 
-        def json(  # type: ignore[misc]  # noqa: F811
+        def json(  # type: ignore[misc]
             self,
             include: Optional[Union['AbstractSetIntStr', 'MappingIntStrAny']] = None,
             exclude: Optional[Union['AbstractSetIntStr', 'MappingIntStrAny']] = None,
